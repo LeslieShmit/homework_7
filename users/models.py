3de21dea3a1cc5587c9b81546f858d491a14adb5
Vladislav Lukin <vladislavlukin090392@gmail.com>
2026-01-31 18:33:56 +0400

    added user app and all necessary settings

---
+++ users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = models.CharField(
+        max_length=15, blank=True, null=True, verbose_name="Номер телефона"
+    )
+    avatar = models.ImageField(
+        upload_to="avatars/", blank=True, null=True, verbose_name="Аватар"
+    )
+    city = models.CharField(
+        max_length=15, blank=True, null=True, verbose_name="Город проживания"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+    def __str__(self):
+        return self.email
+
