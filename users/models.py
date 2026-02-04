97ca4c0295cd54428f884e10f386c0a7f8453bd8
Vladislav Lukin <vladislavlukin090392@gmail.com>
2026-02-05 01:19:35 +0400

    added payment model and migrations

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
+from tkinter.constants import CASCADE
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from materials.models import Course, Lesson
 
 
 class CustomUser(AbstractUser):
@@ -25,7 +29,24 @@ class CustomUser(AbstractUser):
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
-
     def __str__(self):
         return self.email
 
+
+class Payment(models.Model):
+    """
+
+    """
+
+    class PaymentMethod(models.TextChoices):
+        CASH = 'CASH', 'Наличные'
+        BANK = 'BANK', 'Банковский перевод'
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='payment', verbose_name='Пользователь')
+    payment_date = models.DateField(verbose_name='Дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='payment', null=True, blank=True,
+                                    verbose_name='Оплаченный курс')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='payment', null=True, blank=True,
+                                    verbose_name='Оплаченный урок')
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=4, choices=PaymentMethod.choices, verbose_name='Способ оплаты')
