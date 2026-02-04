9935f2b7eac4dc98a4c698b149b8e5414c3971ee
Vladislav Lukin <vladislavlukin090392@gmail.com>
2026-02-05 02:33:28 +0400

    added lessons_detailed field to course serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,17 +2,18 @@ from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-    def get_lessons_count(self, instance):
-        return instance.lessons.count()
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons_detailed = LessonSerializer(source='lessons', many=True)
+
+    class Meta:
+        model = Course
+        fields = ('title', 'preview', 'description', 'lessons_count', 'lessons_detailed')
+
+    def get_lessons_count(self, instance):
+        return instance.lessons.count()
