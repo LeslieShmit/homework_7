9dcaa21f31587e62c135b0689b9a5ddc6243971b
Vladislav Lukin <vladislavlukin090392@gmail.com>
2026-05-09 21:10:42 +0400

    added notification about course update

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,14 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
